d818f52cd8a2fb9b6059128bd601a85175cca9d8
Jacob Buysse <Jacob.Buysse@gmail.com>
2014-12-20 16:10:10 -0600

    Using Wex::Path/File for open.
    Signed-off-by: Jacob Buysse <Jacob.Buysse@gmail.com>

--- ConsoleOpenProject.cpp
+++ ConsoleOpenProject.cpp
@@ -1,6 +1,8 @@
 #include <Wex/WindowsInclude.h>
 #include "ConsoleOpenProject.h"
-#include <stdexcept>
+#include <Wex/Path.h>
+#include <Wex/File.h>
+#include <Wex/Exception.h>
 
 ConsoleOpenProject::ConsoleOpenProject()
 	: ConsoleCommand{ "open", { "project-name" }, "Opens the given project." }
@@ -15,6 +17,15 @@ void ConsoleOpenProject::Execute(const std::vector<std::string>& arguments)
 
 void ConsoleOpenProject::OpenProject(const std::string& projectName)
 {
-	//TODO: open that guy
+	//TODO: allow user to change this
+	const std::string currentDirectory = R"(c:\save\code)";
+
+	auto fullPath = Wex::Path::Combine(currentDirectory, projectName + ".cpp-project");
+	if (!Wex::File::Exists(fullPath))
+		fullPath = Wex::Path::Combine(currentDirectory,
+			projectName + "\\" + projectName + ".cpp-project");
+	CheckError(!Wex::File::Exists(fullPath), 0, "File does not exist.");
+
+	//TODO: something
 }
 
